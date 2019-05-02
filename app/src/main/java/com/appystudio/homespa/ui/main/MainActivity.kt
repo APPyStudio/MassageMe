13267da8aff32f55de4ead6aa4b97a0f1c6efc1a
Mário Rocha <mrocha@mobinteg.com>
2019-05-02 21:33:58 +0100

    Added main, splash and onboarding activities

---
+++ app/src/main/java/com/appystudio/homespa/ui/main/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.appystudio.homespa.ui.main
+
+import com.appystudio.homespa.R
+import com.appystudio.homespa.ui.base.BaseActivity
+
+class MainActivity : BaseActivity() {
+
+    override fun setLayout(): Int = R.layout.activity_main
+
+    override fun requestData() {
+
+    }
+
+    override fun setupView() {
+
+    }
+}
---
+++ app/src/main/java/com/appystudio/homespa/ui/splash/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.appystudio.homespa.ui.splash
+
+import com.appystudio.homespa.R
+import com.appystudio.homespa.ui.base.BaseActivity
+
+class SplashActivity : BaseActivity() {
+
+    override fun setLayout(): Int = R.layout.activity_splash
+
+    override fun requestData() {
+
+    }
+
+    override fun setupView() {
+
+    }
+}
